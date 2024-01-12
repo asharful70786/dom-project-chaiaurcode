32b34d4aab4380105dfc57b45f983a6570260420
ashraful <143166529+asharful70786@users.noreply.github.com>
2024-01-12 22:46:22 +0530

    Update chaiaurcode.js
    java scriot future added

--- 12-randomImage/chaiaurcode.js
+++ 12-randomImage/chaiaurcode.js
@@ -1,3 +1,20 @@
-const baseURL = 'https://source.unsplash.com/all/300x300';
+let btn = document.querySelector("button");
+let random = Math.floor(Math.random() * 5) + 1;
+let p = document.querySelector("p");
 
-// this url gives an image. Use this and NO API calls
+btn.addEventListener("click", () => {
+    let guess = document.getElementById("inp").value;
+
+    if (guess.toLowerCase() === "quit") {
+        p.innerText = `You quit the game. The correct answer was ${random}.`;
+    } else if (guess == random) {
+        p.innerText = `Congratulations! You guessed correctly. The answer is ${random}.`;
+        btn.disabled = true; 
+    }else if(guess<random){
+        p.innerText ="your number is smLL, kindly try a little big"
+    }else if(guess>random){
+        p.innerText ="your number is BIG, kindly try a little small"
+    }else  {
+        p.innerText = `Sorry, your guess was incorrect. Try again.`;
+    }
+});
